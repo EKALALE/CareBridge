a15074836d940cbcf488bdd3e4adad09bc94ee12
Philip Ekalale <philipekalale07@gmail.com>
2025-11-28 23:46:46 +0300

    Models,appointment.py updates and Form

--- careapp/admin.py
+++ careapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from careapp.models import *
+
 
 # Register your models here.
+admin.site.register(Patient)
+admin.site.register(MedicalRecord)
+admin.site.register(Appointment)
